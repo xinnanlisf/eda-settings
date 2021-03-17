3e943698ecbaa10245e692150ee8deb67708e8bd
Xinnan Li <xinnan.li@salesforce.com>
2021-03-17 08:42:13 -0400

    init commit

---
+++ force-app/main/lwc/accountModelSettings/accountModelSettings.js
@@ -0,0 +1,42 @@
+import { LightningElement, api, track } from "lwc";
+import stgAccountModelSettingsTitle from "@salesforce/label/c.stgAccountModelSettingsTitle";
+
+export default class AccountModelSettings extends LightningElement {
+    isEditMode = false;
+    affordancesDisabledToggle = false;
+
+    get affordancesDisabled() {
+        if (!this.isEditMode || this.affordancesDisabledToggle === true) {
+            return true;
+        }
+        return undefined;
+    }
+
+    labelReference = {
+        stgAccountModelSettingsTitle,
+    };
+
+    handleSettingsEditModeChange(event) {
+        this.isEditMode = !event.detail;
+        this.affordancesDisabledToggle = event.detail;
+
+        this.refreshAllApex();
+    }
+
+    handleSettingsSaving(event) {
+        this.affordancesDisabledToggle = true;
+        // TODO: perform client side validation
+
+        // if validation fails, call this.handleValidationFailure()
+        //this.template.querySelector("c-settings-save-canvas").handleValidationFailure();
+
+        // else, update hierarchy settings
+        this.template.querySelector("c-settings-save-canvas").updateHierarchySettings();
+    }
+
+    handleSettingsSaveCompleted(event) {
+        this.affordancesDisabledToggle = false;
+    }
+
+    refreshAllApex() {}
+}
